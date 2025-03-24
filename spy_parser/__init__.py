d5fc07fb3c80e14137420270512ff5888d8253b3
Marcuth <thiago.playgames15@gmail.com>
2025-03-24 15:32:01 -0300

    adiciona classe JsonParser para extração de dados em formato JSON

--- spy_parser/__init__.py
+++ spy_parser/__init__.py
@@ -2,4 +2,5 @@ from .html_parsing_model import HtmlParsingModel, HtmlParsingModelShape, HtmlPar
 from .parsing_model import ParsingModel
 from .json_parsing_model import JsonParsingModel, JsonParsingModelShape, JsonParsingModelShapeValue
 from .html_parser import HtmlParser
+from .json_parser import JsonParser
 from .parser import Parser
